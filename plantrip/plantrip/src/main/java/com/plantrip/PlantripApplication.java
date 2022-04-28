f9e732364b569aa51f71cbe024be260aa2885823
sh <lop9036@naver.com>
2022-04-28 19:57:01 +0900

    #1 부트스트랩 초기 설정

---
+++ plantrip/plantrip/src/main/java/com/plantrip/PlantripApplication.java
@@ -0,0 +1,13 @@
+package com.plantrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantripApplication.class, args);
+	}
+
+}
