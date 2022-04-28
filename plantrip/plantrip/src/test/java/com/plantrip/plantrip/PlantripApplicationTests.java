7beda33d3932a017ae733d2473680105fa440658
sh <lop9036@naver.com>
2022-04-28 11:34:05 +0900

    #0. 초기 환경 설정

---
+++ plantrip/plantrip/src/test/java/com/plantrip/plantrip/PlantripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantrip.plantrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
